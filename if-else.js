e1a72efbbd92b38945bf8b782e872dc7cfaffb15
mharisismail <mharisismail07@gmail.com>
2025-11-23 01:42:32 +0500

    done

--- if-else.js
+++ if-else.js
@@ -10,29 +10,22 @@
 //     alert("Fail")
 // }
 
-
-
-
 // let age = 90
 // let member= true
 
-
 // if(age>=60 && member ==true){
 //     console.log("Senior Premium Discount");
-    
+
 // }else if(age>=60 && member ==false){
 //     console.log("Senior Discount");
-    
+
 // } else if (member == true){
 //     console.log("Premium Discount");
-    
+
 // }else{
 //     console.log("No Discount");
-    
+
 // }
-
-
-
 
 // let login = false
 
@@ -40,22 +33,18 @@
 
 // if(login===true && verification ===true){
 //     console.log( "Access granted");
-    
+
 // }else if(login===true && verification === false){
 //     console.log("Please verify your email!!");
-    
+
 // }else{
 //     console.log("Please log in!!");
-    
+
 // }
-
-
-
 
 // let age = 55;
 // let country = "india";
 // let ban = false;
-
 
 // if(ban==true){
 //     alert("You are not eligible because you are banned.");
@@ -66,30 +55,51 @@
 // }else {
 //      alert(" You are eligible for the contest!");
 // }
-    
-
-
-
 
 // let plan = "premium";
 // let login = "no" ;
 
 // if(plan === "premium" && login==="yes" ){
 //     console.log("Welcome Premium User");
-    
+
 // }else if ( plan=== "free" && login === "yes"){
 //     console.log("Welcome Free User");
-    
+
 // }else if (login==="no"){
 //     console.log("Please log in to continue");
-    
+
 // }else{
 //     console.log("Invalid subscription");
-    
+
 // }
 
+// let price  = 10;
+// let stock = 10;
+// let member = true;
 
+// if( price>=1000 && stock>0 && member){
+//     console.log("Buy now with discount");
 
+// }else if (stock===0 ){
+//     console.log("Out of stock");
+
+// }else{
+//     console.log("Invalid product");
+
+// }
+
+// let hours = 40;
+// let weekend = false;
+
+// if (hours >= 40 && weekend == false) {
+//   console.log("Overtime!!!");
+// } else if (hours >= 0 && weekend == true) {
+//   console.log("Weekend Work");
+// } else if (hours > 0 && hours <= 40) {
+//   console.log("regular Work");
+// } else if (hours === 0) {
+//   console.log("no work");
+// }
 
 
 
