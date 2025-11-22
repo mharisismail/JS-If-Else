90e1b37a84afac3cdcfb27437f06f113a0079ec7
mharisismail <mharisismail07@gmail.com>
2025-11-23 01:05:04 +0500

    half work done

---
+++ app.js
@@ -0,0 +1,325 @@
+// let temp = +prompt("enter temp");
+
+// if(temp <= 10){
+//     console.log("jackeet pehnly Bahi!!!!!");
+    
+// } else if (temp >10 && temp <=20){
+//     console.log("sweater hi theak hia bahi!!!!");
+    
+// }else if (temp >20 && temp <= 30 ){
+//     console.log(" Bahi T-shirt pehn k ana ");
+    
+// }else{
+//     console.log("kia kar rha hia normal kapry pehnly bahi!!");
+    
+// }
+
+
+
+
+// let active = prompt("kiya kam karty ho ")
+
+// if(active === "Sedentary"){
+//     console.log("2 liter pani pe ly");
+// }  else if ( active === "Moderate"){
+//     console.log("2.5 liters peelo");
+    
+// } else{
+//     console.log("3 liters pee ly bahi ");
+    
+// } 
+
+
+
+// let sub1 = +prompt("subject 1");
+
+// let sub2 = +prompt("subject 2");
+
+// let sub3 = +prompt("subject 3");
+
+// let result = sub1 + sub2+ sub3;
+
+
+
+
+// if(result  >= 40){
+//     console.log("Bahi ap pass ho");
+    
+// }else{
+//     console.log("Nalaik ho tum");
+// }
+
+
+
+// let time = 18
+
+
+// if( time>= 6 && time<= 17 ){
+//     console.log("light mode on");
+    
+// }else{
+//     console.log(" dark mode on");
+    
+// }
+
+
+
+
+
+
+// let time = 11
+
+// if( time >=6 && time<= 10){
+//     console.log("Breakfasr time ");
+    
+// } else if (  time >= 12 &&  time >=14 ){
+//     console.log(" Lunch Time ");
+    
+// } else if (time >= 18 &&  time >=21){
+//     console.log("dinner time ");
+    
+// } else{
+//     console.log("Snack Time");
+    
+// }
+
+
+
+
+
+// let age = +prompt("Enter Your Age!!!!")
+
+// if(age >= 18){
+//     alert("You'r An Adult ");
+    
+// }else{
+//     alert("You'r A Miner")
+// }
+
+
+
+
+// let number = +prompt("Enter The Number ???");
+
+
+// if(number>= 0){
+//     console.log("Positive Number ");
+    
+// }else{
+//     console.log("Negative Number");
+    
+// }
+
+
+
+
+// let number = +prompt("Enter A Number ")
+
+
+
+// if(number % 2===0){
+//     alert("Even Number ")
+// }else{
+//     alert("Odd Number")
+// }
+
+
+
+
+// let username = prompt("Enter Your UserName ??")
+
+// if( username === "admin"){
+//     alert("Welcome Admin!!!")
+// }else{
+//     alert("Access denied.")
+// }
+
+
+
+
+
+// let num1  = +prompt("enter A number")
+// let num2  = +prompt("enter A number")
+
+// if(num1 === num2){
+//     alert("The Numbers Are Same")
+// }else{
+//     alert("The Numbers Are Not Same!!!")
+// }
+
+
+
+// let num1 = +prompt("Enter A Number");
+
+// let num2 = +prompt("Enter A Number");
+
+
+// if(num1 > num2){
+//     alert("First is larger .")
+// }else{
+//     alert("Second is larger .")
+// }
+
+
+
+
+// let number = +prompt("Enter A Number ")
+
+
+// if(number>100){
+//     alert("TOO BIGGG!!!!!!!!")
+// }else{
+//     alert("It is in Range")
+// }
+
+
+
+
+// let score = +prompt("Whats Your Score In the Game ")
+
+
+// if( score >= 50){
+//     console.log("You Passed!");
+    
+// }else{
+//     console.log("You Failed!!!!");
+    
+// }
+
+
+
+// let temp = +prompt("Whats The Temprature OutSide?")
+
+
+// if( temp >= 30){
+//     alert("It's Hot Outside!!!!!!")
+// } else{
+//     alert("It's Not Hot Outside")
+// }
+
+
+
+
+// let num = prompt("Whats Number In your Mind??");
+
+// if(num===0){
+//     alert("It's ZEROO")
+// }else {
+//     alert("It's Not Zeroo")
+// }
+
+
+
+// let age = +prompt("Enter Your Age!")
+
+
+// if(age>=60){
+//     alert("Hey! Senior Citizen")
+// }else{
+//     alert("Hey! Young Gun")
+// }
+
+
+
+
+// let name = prompt("What's Your Name?");
+
+// if(name === "Alice"){
+//     alert("HI! Alice")
+// }else{
+//     alert("OH!! Your Not Alice")
+// }
+
+
+
+
+// let pass = +prompt("Enter Your Password!");
+
+
+// if(pass==1234){
+//     console.log("Correct Password");
+    
+// }else{
+//     console.log("Wrong Password");
+    
+// }
+
+
+
+
+// let time = +prompt("What Time is it??");
+
+// if(time<= 12){
+//     alert("Good Morning!.")
+// }else{
+//     alert("Good Afternoon!.")
+// }
+
+
+
+// let num = +prompt("Enter A Number");
+
+
+// if(num % 5 ===0){
+//     console.log("Divisible by 5");
+// }else{
+//     console.log("Not divisible by 5");
+    
+// }
+
+
+
+
+
+
+// let game = prompt(" Do You want to play a game? (yes/no).");
+
+// if(game=="yes"){
+//     alert("Let's play!")
+// }else{
+//     alert("Maybe next time!")
+// }
+
+
+
+// let color = prompt("Enter The Color You Like!");
+
+
+// if(color=="blue"){
+//     console.log("Cool choice!");
+    
+// }else{
+//     console.log("Interesting color!");
+    
+// }
+
+
+
+
+// let day = prompt("What's The Day?");
+
+
+// if(day=="Sunday"){
+//     alert("Weekend!!!!")
+// }else{
+//     alert("Weekday")
+// }
+
+
+
+// let fruit = prompt("What's Your Favourite Fruit?");
+
+// if(fruit==="apple" || fruit ==="banana"){
+//     alert("We Have That!!!!")
+// }else{
+//     alert("Sorry, Out Of Stock!")
+// }
+
+
+// let game = prompt("Do You Want to Start The Game? (YES/NO)");
+
+// if( game==="yes"){
+//     alert("Start The Game")
+// }else{
+//     alert("Waiting for input...")
+// }
---
+++ if-else.js
@@ -0,0 +1,96 @@
+// let marks= +prompt("Enter Your Marks");
+
+// if(marks<=0 || marks>=100){
+//     alert("Invalid Marks")
+// }else if( marks >= 80){
+//     alert("Distinction")
+// } else if ( marks >=50 ){
+//     alert("Pass")
+// }else{
+//     alert("Fail")
+// }
+
+
+
+
+// let age = 90
+// let member= true
+
+
+// if(age>=60 && member ==true){
+//     console.log("Senior Premium Discount");
+    
+// }else if(age>=60 && member ==false){
+//     console.log("Senior Discount");
+    
+// } else if (member == true){
+//     console.log("Premium Discount");
+    
+// }else{
+//     console.log("No Discount");
+    
+// }
+
+
+
+
+// let login = false
+
+// let verification = false
+
+// if(login===true && verification ===true){
+//     console.log( "Access granted");
+    
+// }else if(login===true && verification === false){
+//     console.log("Please verify your email!!");
+    
+// }else{
+//     console.log("Please log in!!");
+    
+// }
+
+
+
+
+// let age = 55;
+// let country = "india";
+// let ban = false;
+
+
+// if(ban==true){
+//     alert("You are not eligible because you are banned.");
+// }else if ( age <18 && age >35){
+//     alert ("You are not eligible because your age is not within 18–35.");
+// }else if ( country !=="india" && country !=="pakistan"){
+//     alert ("You are not eligible because you are not from Pakistan or India.")
+// }else {
+//      alert(" You are eligible for the contest!");
+// }
+    
+
+
+
+
+// let plan = "premium";
+// let login = "no" ;
+
+// if(plan === "premium" && login==="yes" ){
+//     console.log("Welcome Premium User");
+    
+// }else if ( plan=== "free" && login === "yes"){
+//     console.log("Welcome Free User");
+    
+// }else if (login==="no"){
+//     console.log("Please log in to continue");
+    
+// }else{
+//     console.log("Invalid subscription");
+    
+// }
+
+
+
+
+
+
+
